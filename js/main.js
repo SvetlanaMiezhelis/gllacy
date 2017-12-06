85c6695783b0d4f4c42250592b0504df9cc6bfc3
USER\flerka.kazama <fleurkazama@gmail.com>
2017-12-06 18:17:29 +0300

    карта

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function initMap() {
+
+
+var map = new google.maps.Map(document.getElementById('map'), {
+	zoom: 16,
+	center: {lat: 59.938809, lng: 30.332294}
+});
+var marker = new google.maps.Marker({
+	position: {lat: 59.938809, lng: 30.323342},
+	map: map,
+
+	icon: "../img/pin.svg"
+});
+
+
+
+}
+
+
+google.maps.event.addDomListener(window, "load", initMap);
