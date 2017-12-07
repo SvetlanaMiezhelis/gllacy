5ccfa3b96fde2ce2d48084122afa0579d9022970
USER\flerka.kazama <fleurkazama@gmail.com>
2017-12-08 02:09:24 +0300

    декоративное оформление,исправление ошибок

--- js/main.js
+++ js/main.js
@@ -16,3 +16,44 @@ function initMap() {
 google.maps.event.addDomListener(window, "load", initMap);
 
 
+
+var link = document.querySelector(".feedback-form-open");
+
+var popup = document.querySelector(".modal-form");
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector("form");
+var name = popup.querySelector("[name=feedback-name]");
+var email = popup.querySelector("[name=email]");
+
+link.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-show");
+	popup.querySelector("[name=feedback-name]").focus();
+});
+
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+	if (!name.value || !email.value) {
+		evt.preventDefault();
+		popup.classList.remove("modal-error");
+		popup.offsetWidth = popup.offsetWidth;
+		popup.classList.add("modal-error");
+	}
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		if (popup.classList.contains("modal-show")) {
+			popup.classList.remove("modal-show");
+			popup.classList.remove("modal-error");
+		}
+	}
+});
+
+
