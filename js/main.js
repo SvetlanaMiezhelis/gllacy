281e549c2104fd4dfdddd91532414ad81fe64510
USER\flerka.kazama <fleurkazama@gmail.com>
2017-12-10 21:07:34 +0300

    визуальное оформление,правки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function initMap() {
 
-	var map = new google.maps.Map(document.getElementById('map'), {
+	var map = new google.maps.Map(document.getElementById("map"), {
 		zoom: 16,
 		center: {lat: 59.938809, lng: 30.332294}
 	});
@@ -25,17 +25,20 @@ var close = popup.querySelector(".modal-close");
 var form = popup.querySelector("form");
 var name = popup.querySelector("[name=feedback-name]");
 var email = popup.querySelector("[name=email]");
+var overlay = document.querySelector(".feedback-form-overlay");
 
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.add("modal-show");
 	popup.querySelector("[name=feedback-name]").focus();
+	overlay.classList.add("overlay-show");
 });
 
 close.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.remove("modal-show");
 	popup.classList.remove("modal-error");
+	overlay.classList.remove("overlay-show");
 });
 
 form.addEventListener("submit", function (evt) {
